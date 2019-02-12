c9f0f752be0a6ce6c744f0013760c335007aa235
Stephen Holdaway <stecman@gmail.com>
2019-02-12 23:51:04 +1300

    Reduce compiled size of uart_read_byte function by 18 bytes
    - Half-bit delay is now 49uS as this generates a fewer instructions for the
      configured clock frequency (no padding with nops)
    
    - Packed stopbit delay into loop to avoid two inlined _delay_us calls.
    
    This makes the size of this routine on-par with AVR ASM implementations
    I've seen around.

--- firmware/softuart.c
+++ firmware/softuart.c
@@ -5,7 +5,7 @@
 
 // Number of microseconds for a 0.5 bit delay at the desired baud rate
 // Calculated as ((1/BPS)/0.000001)/2
-#define UART_DELAY_US 50 // 9600 baud
+#define UART_DELAY_US 49 // ~9600 baud
 
 /**
  * Read a single byte transmitted by the GPS
@@ -20,24 +20,27 @@ uint8_t uart_read_byte()
     // 0.5 bit delay
     _delay_us(UART_DELAY_US);
 
-    // Shift 8 data bits in at the baud-rate determined by UART_DELAY_MS
-    uint8_t bit = 8;
+    // Shift 8 data bits + 1 stop bit at the baud-rate determined by UART_DELAY_MS
+    uint8_t bit = 9;
     do {
         --bit;
-        data >>= 1;
 
         // 1 bit delay
         _delay_us(UART_DELAY_US);
         _delay_us(UART_DELAY_US);
 
+        // If this is the stop-bit, don't try to store the value
+        if (bit == 0) {
+            break;
+        }
+
+        // Shift to make room for next bit (reading most-significant bit first)
+        data >>= 1;
+
         if ((PINB & _BV(PIN_SOFT_RX)) != 0) {
             data |= 0x80;
         }
     } while (bit != 0);
-
-    // 1 bit delay for stop-bit
-    _delay_us(UART_DELAY_US);
-    _delay_us(UART_DELAY_US);
 
     return data;
 }
